b2176075eb418200a081d63666c4c5fc93e97e36
Tejsharma15 <77492919+Tejsharma15@users.noreply.github.com>
2022-11-25 01:46:38 +0530

    Update testFail.py

--- testFail.py
+++ testFail.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import unittest
-from program import prog
+from program import isOdd
 class TestCase(unittest.TestCase):
     def testcase1(self):
-        self.assertEqual(prog(4), 1)
+        self.assertEqual(isOdd(4), 1)
     def testcase2(self):
-        self.assertEqual(prog(6), 1)
+        self.assertEqual(isOdd(6), 1)
     def testcase3(self):
-        self.assertEqual(prog(86), 1)
+        self.assertEqual(isOdd(86), 1)
 
 if __name__ == '__main__':
     unittest.main()
